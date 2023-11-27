d0d0bb471506b2e70d64889016eccf05822fe2a3
gorkemuveyk <uveykgorkem@gmail.com>
2023-11-27 18:01:03 +0300

    created sidebar

---
+++ src/layouts/sidebar/menu/new/index.jsx
@@ -0,0 +1,11 @@
+import Button from "../../../../components/buttons";
+
+const New = () => {
+  return (
+    <div className="py-4 w-[90%]">
+      <Button size={"large"}>Post</Button>
+    </div>
+  );
+};
+
+export default New;
